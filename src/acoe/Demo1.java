ba73969e07eacfb22a4028d1482cfb529669028b
sriva <sriva@LAPTOP-1K87IB6O>
2022-10-14 13:07:11 +0530

    first commot

---
+++ src/acoe/Demo1.java
@@ -0,0 +1,11 @@
+package acoe;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("hello welcome to devops lab");
+		// TODO Auto-generated method stub
+
+	}
+
+}
