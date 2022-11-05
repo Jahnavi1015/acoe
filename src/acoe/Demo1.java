9ab852d1141feef15f6258e8b2f8f0cf537b142d
Jahnavi1015 <82027849+Jahnavi1015@users.noreply.github.com>
2022-11-05 12:40:54 +0530

    Update Demo1.java
    pull

--- src/acoe/Demo1.java
+++ src/acoe/Demo1.java
@@ -4,6 +4,7 @@ public class Demo1 {
 
 	public static void main(String[] args) {
 		System.out.println("hello welcome to devops lab");
+                System.out.println("jahnavi and srivalli");
 		// TODO Auto-generated method stub
 
 	}
